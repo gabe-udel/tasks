bc81fd12a867a6f134241d9504ef68f6af469e5e
Gabe Vandevere <gvand@udel.edu>
2026-02-07 21:40:38 -0500

    Added name to app

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,10 @@ function App(): React.JSX.Element {
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload.
             </p>
+
+            <p>
+                Hi, its Gabe Vandevere.
+            </p>
         </div>
     );
 }
