c15ee36c18a7a8c14f88e972c76fab9455fbd256
Gabe Vandevere <gvand@udel.edu>
2026-02-15 21:33:38 -0500

    added src/images folder and updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import "./App.css";
+import img from "./images/dolphin.jpeg";
+import { Button, Container, Row, Col } from "react-bootstrap";
 
 function App(): React.JSX.Element {
     return (
@@ -7,12 +9,35 @@ function App(): React.JSX.Element {
             <header className="App-header">
                 UD CISC275 with React Hooks and TypeScript
             </header>
-            <p>
-                Edit <code>src/App.tsx</code> and save. This page will
-                automatically reload.
-            </p>
+            <h1>This is header text</h1>
+            <img
+                src={img}
+                alt="Here is an image of a complacent-looking dolphin."
+            ></img>
 
-            <p>Hello World, its Gabe Vandevere</p>
+            <ol>
+                <li>First thing</li>
+                <li>Another thing</li>
+                <li>A third item</li>
+            </ol>
+
+            <Button
+                onClick={() => {
+                    console.log("Hello World!");
+                }}
+            >
+                Log Hello World
+            </Button>
+
+            <Container
+                id="container"
+                style={{ height: 30, width: 30, backgroundColor: "red" }}
+            >
+                <Row>
+                    <Col>First column.</Col>
+                    <Col>Second column.</Col>
+                </Row>
+            </Container>
         </div>
     );
 }
