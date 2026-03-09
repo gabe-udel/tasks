e453eafcad12c94c0bcaad6b8cd9ab958533d013
Gabe Vandevere <gvand@udel.edu>
2026-03-08 22:52:20 -0400

    finished task 9

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,10 @@
 import React from "react";
 import "./App.css";
-import { ChangeType } from "./components/ChangeType";
-import { RevealAnswer } from "./components/RevealAnswer";
-import { StartAttempt } from "./components/StartAttempt";
-import { TwoDice } from "./components/TwoDice";
-import { CycleHoliday } from "./components/CycleHoliday";
-import { Counter } from "./components/Counter";
-import { DoubleHalf } from "./bad-components/DoubleHalf";
-import { ColoredBox } from "./bad-components/ColoredBox";
+
+// import { DoubleHalf } from "./bad-components/DoubleHalf";
+// import { ColoredBox } from "./bad-components/ColoredBox";
 import { ShoveBox } from "./bad-components/ShoveBox";
-import { ChooseTeam } from "./bad-components/ChooseTeam";
+// import { ChooseTeam } from "./bad-components/ChooseTeam";
 
 function App(): React.JSX.Element {
     return (
@@ -20,23 +15,11 @@ function App(): React.JSX.Element {
             <hr></hr>
             {/* <DoubleHalf></DoubleHalf> */}
             <hr></hr>
-            <ChooseTeam></ChooseTeam>
+            {/* <ChooseTeam></ChooseTeam> */}
             <hr></hr>
-            <ColoredBox></ColoredBox>
+            {/* <ColoredBox></ColoredBox> */}
             <hr></hr>
             <ShoveBox></ShoveBox>
-            <hr></hr>
-            <Counter></Counter>
-            <hr />
-            <RevealAnswer></RevealAnswer>
-            <hr />
-            <StartAttempt></StartAttempt>
-            <hr />
-            <TwoDice></TwoDice>
-            <hr />
-            <ChangeType></ChangeType>
-            <hr />
-            <CycleHoliday></CycleHoliday>
         </div>
     );
 }
