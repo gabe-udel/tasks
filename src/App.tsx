6a9326ab0bdd84dd710914f51e2671b579d574d6
Gabe Vandevere <gvand@udel.edu>
2026-03-08 22:52:53 -0400

    uncommented out components in app - final task 9 submission

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import "./App.css";
 
-// import { DoubleHalf } from "./bad-components/DoubleHalf";
-// import { ColoredBox } from "./bad-components/ColoredBox";
+import { DoubleHalf } from "./bad-components/DoubleHalf";
+import { ColoredBox } from "./bad-components/ColoredBox";
 import { ShoveBox } from "./bad-components/ShoveBox";
-// import { ChooseTeam } from "./bad-components/ChooseTeam";
+import { ChooseTeam } from "./bad-components/ChooseTeam";
 
 function App(): React.JSX.Element {
     return (
@@ -13,11 +13,11 @@ function App(): React.JSX.Element {
                 UD CISC275 with React Hooks and TypeScript
             </header>
             <hr></hr>
-            {/* <DoubleHalf></DoubleHalf> */}
+            <DoubleHalf></DoubleHalf>
             <hr></hr>
-            {/* <ChooseTeam></ChooseTeam> */}
+            <ChooseTeam></ChooseTeam>
             <hr></hr>
-            {/* <ColoredBox></ColoredBox> */}
+            <ColoredBox></ColoredBox>
             <hr></hr>
             <ShoveBox></ShoveBox>
         </div>
